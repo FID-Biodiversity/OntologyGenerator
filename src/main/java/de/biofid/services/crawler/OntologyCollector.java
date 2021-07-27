1b384f229272785f956a8674d789cfd87c484a94
Adrian Pachzelt <a.pachzelt@ub.uni-frankfurt.de>
2021-07-27 14:50:14 +0200

    Added logger configuration

--- src/main/java/de/biofid/services/crawler/OntologyCollector.java
+++ src/main/java/de/biofid/services/crawler/OntologyCollector.java
@@ -5,10 +5,14 @@ import de.biofid.services.configuration.OntologyConfiguration;
 import de.biofid.services.configuration.reader.JsonConfigurationReader;
 import de.biofid.services.factories.ServiceFactory;
 import de.biofid.services.ontologies.OntologyGenerator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public class OntologyCollector {
+
+    protected static final Logger logger = LogManager.getLogger();
 
     private static final String HARVESTER_CONFIGURATION_FILE_PATH = "config/general.json";
 
@@ -17,6 +21,9 @@ public class OntologyCollector {
     }
 
     public static void start() {
+        logger.info("Starting Ontology Collector!");
+        logger.info("Looking for configuration data in \"" + HARVESTER_CONFIGURATION_FILE_PATH + "\".");
+
         Configuration configuration = new Configuration(new JsonConfigurationReader());
         configuration.loadFromFile(HARVESTER_CONFIGURATION_FILE_PATH);
 
